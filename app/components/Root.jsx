c49a18672e6a2d76f89f6cae8cc1e06b4b6e9166
Florian Witteler <florian_witteler@gmx.de>
2019-01-07 22:51:03 +0100

    drawing moon and earth to scale.
    you have to scroll around a bit to actually find the moon.
    
    Rotation of the planets not correct yet.

--- app/components/Root.jsx
+++ app/components/Root.jsx
@@ -27,25 +27,25 @@ renderer.setSize(WIDTH, HEIGHT);
 const VIEW_ANGLE = 45;
 const ASPECT = WIDTH / HEIGHT;
 const NEAR = 0.1;
-const FAR = 10000;
+const FAR = 30000 * 32.39;
 
 //create a camera
 const camera =
-new THREE.PerspectiveCamera(
-    VIEW_ANGLE,
-    ASPECT,
-    NEAR,
-    FAR
-);
+    new THREE.PerspectiveCamera(
+        VIEW_ANGLE,
+        ASPECT,
+        NEAR,
+        FAR
+    );
 
 //set the camera position - x, y, z
-camera.position.set( 0, 0, 500 );
+camera.position.set(0, 0, 3000 * 32.39);
 
 // Create a scene
 const scene = new THREE.Scene();
 
 //set the scene background
-scene.background = new THREE.Color( 0x000 );
+scene.background = new THREE.Color(0x000);
 
 //add the camera to the scene.
 scene.add(camera);
@@ -56,54 +56,93 @@ container.appendChild(renderer.domElement);
 //Three.js uses geometric meshes to create primitive 3D shapes like spheres, cubes, etc. I’ll be using a sphere.
 
 // Set up the sphere attributes
-const RADIUS = 200;
-const SEGMENTS = 50;
-const RINGS = 50;
+const EARTH_RADIUS = 6378;
+const MOON_RADIUS = 1737;
+const SEGMENTS = 100;
+const RINGS = 100;
 
 //Create a group (which will later include our sphere and its texture meshed together)
-const globe = new THREE.Group();
+const miniUniverse = new THREE.Group();
 //add it to the scene
-scene.add(globe);
+scene.add(miniUniverse);
 
 //Let's create our globe using TextureLoader
 
 // instantiate a loader
 var loader = new THREE.TextureLoader();
-loader.load( 'land_ocean_ice_cloud_2048.jpg', function ( texture ) {
-    //create the sphere
-    var sphere = new THREE.SphereGeometry( RADIUS, SEGMENTS, RINGS );
 
-    //map the texture to the material. Read more about materials in three.js docs
-    var material = new THREE.MeshBasicMaterial( { map: texture, overdraw: 0.5 } );
+function addSphere(radius, textureFilename, coord3d) {
+    loader.load(textureFilename, function (texture) {
+        //create the sphere
+        var sphere = new THREE.SphereGeometry(radius, SEGMENTS, RINGS);
 
-    //create a new mesh with sphere geometry. 
-    var mesh = new THREE.Mesh( sphere, material );
+        //map the texture to the material. Read more about materials in three.js docs
+        var material = new THREE.MeshBasicMaterial({ map: texture, overdraw: 0.5 });
 
-    //add mesh to globe group
-    globe.add(mesh);
-} );
+        //create a new mesh with sphere geometry. 
+        var mesh = new THREE.Mesh(sphere, material);
+
+        mesh.position.x = coord3d.x;
+        mesh.position.y = coord3d.y;
+        mesh.position.z = coord3d.z;
+
+        //add mesh to universe group
+        miniUniverse.add(mesh);
+    });
+}
+
+//const earthTexture = 'land_ocean_ice_cloud_2048.jpg';
+const earthTexture = 'earth_no_clouds_4k.jpg';
+const moonTexture = 'moon_4k.jpg';
+
+
+const DISTANCE_MOON_EARTH = 384402;
+addSphere(EARTH_RADIUS, earthTexture, { x: 0, y: 0, z: 0 });
+
+var moonCoords = get3dCoordsOfGpsCoord(EARTH_RADIUS, DISTANCE_MOON_EARTH - EARTH_RADIUS, degrees_to_radians(-7.09), degrees_to_radians(-150.3));
+console.log("moonCoords", moonCoords)
+addSphere(MOON_RADIUS, moonTexture, moonCoords)
+
 
 // Move the sphere back (z) so we can see it.
-globe.position.z = -300;
+//miniUniverse.position.z = -700 * 32.39; //0; //-300;
 
 //Lighting
 
 //create a point light (won't make a difference here because our material isn't affected by light)
 const pointLight =
-new THREE.PointLight(0xFFFFFF);
+    new THREE.PointLight(0xFFFFFF);
 
 //set its position
-pointLight.position.x = 10;
-pointLight.position.y = 50;
-pointLight.position.z = 400;
+pointLight.position.x = 320;
+pointLight.position.y = 1600;
+pointLight.position.z = 12800;
 
 //add light to the scene
 scene.add(pointLight);
 
+function degrees_to_radians(degrees) {
+    var pi = Math.PI;
+    return degrees * (pi / 180);
+}
+
+function get3dCoordsOfGpsCoord(earthRadiusInKilometers, alt, latInRadians, lonInRadians) {
+    var r = earthRadiusInKilometers + alt;
+    var x = r * Math.cos(latInRadians) * Math.cos(lonInRadians);
+    var y = r * Math.cos(latInRadians) * Math.sin(lonInRadians);
+    var z = r * Math.sin(latInRadians);
+
+    return {
+        x: x,
+        y: y,
+        z: z
+    };
+}
+
 //Update
 
 //set update function to transform the scene and view
-function update () {
+function update() {
     //render
     renderer.render(scene, camera);
 
@@ -122,16 +161,16 @@ function animationBuilder(direction) {
         //based on key pressed, rotate +-x or +-y
         switch (direction) {
             case 'up':
-                globe.rotation.x -= 0.2;
+                miniUniverse.rotation.x -= 0.2;
                 break;
             case 'down':
-                globe.rotation.x += 0.2;
+                miniUniverse.rotation.x += 0.2;
                 break;
             case 'left':
-                globe.rotation.y -= 0.2;
+                miniUniverse.rotation.y -= 0.2;
                 break;
             case 'right':
-                globe.rotation.y += 0.2;
+                miniUniverse.rotation.y += 0.2;
                 break;
             default:
                 break;
@@ -175,19 +214,19 @@ document.onkeydown = checkKey;
 //Rotate on Mouse Move
 
 //store our previous mouse move; start value is at center
-var lastMove = [window.innerWidth/2, window.innerHeight/2];
+var lastMove = [window.innerWidth / 2, window.innerHeight / 2];
 
 //callback function for mouse move event listener
 function rotateOnMouseMove(e) {
     e = e || window.event;
 
     //calculate difference between current and last mouse position
-    const moveX = ( e.clientX - lastMove[0]);
-    const moveY = ( e.clientY - lastMove[1]);
+    const moveX = (e.clientX - lastMove[0]);
+    const moveY = (e.clientY - lastMove[1]);
 
     //rotate the globe based on distance of mouse moves (x and y)
-    globe.rotation.y += ( moveX * .005);
-    globe.rotation.x += ( moveY * .005);
+    miniUniverse.rotation.y += (moveX * .005);
+    miniUniverse.rotation.x += (moveY * .005);
 
     //store new position in lastMove
     lastMove[0] = e.clientX;
